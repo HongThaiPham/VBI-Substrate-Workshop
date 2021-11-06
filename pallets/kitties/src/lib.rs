37f5bfffaf8fe9b78eb1feed822232e238942904
Leo Pham <hongthaipro@gmail.com>
2021-11-06 14:03:13 +0700

    Scaffold Kitty struct

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -2,21 +2,44 @@
 
 pub use pallet::*;
 
+// We'll need to add TypeInfo in order to give our struct access to this trait.
+use scale_info::TypeInfo;
+
 #[frame_support::pallet]
 pub mod pallet {
-    use frame_support::{sp_runtime::traits::{Hash, Zero},
-                        dispatch::{DispatchResultWithPostInfo, DispatchResult},
-                        traits::{Currency, ExistenceRequirement, Randomness},
-                        pallet_prelude::*};
+	use frame_support::pallet_prelude::*;
     use frame_system::pallet_prelude::*;
+    use frame_support::{
+        sp_runtime::traits::Hash,
+        traits::{ Randomness, Currency, tokens::ExistenceRequirement },
+        transactional
+    };
     // use sp_io::hashing::blake2_128;
 
-    // TODO Part II: Struct for holding Kitty information.
+	#[cfg(feature = "std")]
+    use serde::{Deserialize, Serialize};
+
+    // Struct for holding Kitty information.
+	type AccountOf<T> = <T as frame_system::Config>::AccountId;
+	type BalanceOf<T> =
+		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	// Struct for holding Kitty information.
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo)]
+	#[scale_info(skip_type_params(T))]
+	pub struct Kitty<T: Config> {
+		pub dna: [u64; 128], // Kitty must have a 128 bit DNA
+		pub price: Option<BalanceOf<T>>,
+		pub gender: Gender,
+		pub owner: AccountOf<T>,
+	}
+
+
 
     // TODO Part II: Enum and implementation to handle Gender type in Kitty struct.
 
     #[pallet::pallet]
-    #[pallet::generate_store(trait Store)]
+    #[pallet::generate_store(pub(super) trait Store)]
     pub struct Pallet<T>(_);
 
     /// Configure the pallet by specifying the parameters and types it depends on.
@@ -46,7 +69,7 @@ pub mod pallet {
 
     // ACTION: Storage item to keep a count of all existing Kitties.
 	#[pallet::storage]
-	#[pallet::getter(fn kitty_cnt)]
+	#[pallet::getter(fn all_kitties_count)]
 	/// Keeps track of the number of Kitties in existence.
 	pub(super) type KittyCnt<T: Config> = StorageValue<_, u64, ValueQuery>;
 
